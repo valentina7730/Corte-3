d431bd3d154347873413367f7ac5ad77c68d7b33
peñosky <jd.pena06@ciaf.edu.co>
2025-11-22 02:02:27 -0500

    pantalla carga

--- my-auth-app-react/src/components/Register.jsx
+++ my-auth-app-react/src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { useEffect } from "react";
 const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001";
 
 export const Register = () => {
@@ -12,11 +12,23 @@ export const Register = () => {
 	const [loading, setLoading] = useState(false);
 	const [message, setMessage] = useState(null);
 
+   useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+        setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+}, []); 
+
+    //extrae name y value del evento | actualiza el estado del formulario
 	const handleChange = (e) => {
 		const { name, value } = e.target;
 		setForm((prev) => ({ ...prev, [name]: value }));
 	};
 
+
+    //construye dinámicamente una URL de endpoint, no duplicar barras / y de que exista  /api/v1.
 	const buildEndpoint = () => {
 		const base = API_URL.replace(/\/$/, "");
 		const prefix = base.endsWith("/api/v1") ? base : `${base}/api/v1`;
@@ -60,8 +72,23 @@ export const Register = () => {
 		}
 	};
 
+    
+    if (loading) {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="flex flex-col items-center gap-4">
+        <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-blue-600"></div>
+        <p className="text-gray-700 font-semibold text-lg">Cargando, por favor espera...</p>
+      </div>
+    </div>
+  );
+}
+
 	return (
-		<div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+
+
+		<div className=" flex items-center justify-center h-screen bg-gray-100">
+
 			<div className="w-full max-w-md bg-white rounded-lg shadow-md p-6">
 				<h2 className="text-2xl font-semibold mb-4">Crear cuenta</h2>
 
