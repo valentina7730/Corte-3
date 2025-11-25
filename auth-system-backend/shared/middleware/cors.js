fbbab079aba021c46ebf2a62b0de71469b9de063
valentinaquinterogodoy <quinterovalentina52@gmail.com>
2025-11-25 16:40:05 -0500

    correccion

--- auth-system-backend/shared/middleware/cors.js
+++ auth-system-backend/shared/middleware/cors.js
@@ -3,7 +3,7 @@ const corsMiddleware = (req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  // Muy importante: responder OK al preflight
+  
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
